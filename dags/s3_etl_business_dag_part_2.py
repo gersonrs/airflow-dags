b0029622aa4d675415a0b79949fd1cfa0314c3ed
Gerson <gersonrodriguessantos8@gmail.com>
2025-01-25 19:41:56 -0300

    feat: adjust code

--- dags/s3_etl_business_dag_part_2.py
+++ dags/s3_etl_business_dag_part_2.py
@@ -11,8 +11,8 @@ from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.utils.dates import days_ago
 
+from dags.src.s3_etl_business import read_business_json_data
 from dags.utils.constants import PROCESSING_ZONE
-from src.s3_etl_business import read_business_json_data
 
 # [START import_module]
 # [START env_variables]
