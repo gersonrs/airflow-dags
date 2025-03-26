a7662ca4e06df306f855d0d8d59092c770b04ece
Gerson <gersonrodriguessantos8@gmail.com>
2025-03-26 20:36:51 -0300

    feat: delete uber data metadata store

--- dags/spark/jobs/show_case_uber_eats_gold_dataset.py
+++ dags/spark/jobs/show_case_uber_eats_gold_dataset.py
@@ -5,9 +5,10 @@ import random
 
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr
-from pyspark.sql.functions import lit
 from pyspark.sql.functions import udf
+
+# from pyspark.sql.functions import expr
+# from pyspark.sql.functions import lit
 
 # main spark program
 if __name__ == "__main__":
@@ -22,7 +23,9 @@ if __name__ == "__main__":
 
     spark.sql("SHOW TABLES IN uber").show()
 
-    df = spark.table("uber.silver_deliveries")
+    spark.sql("DROP DATABASE IF EXISTS uber CASCADE")
+
+    # df = spark.table("uber.silver_deliveries")
 
     @udf("int")
     def age_from_birth(date_birth: str) -> int | None:
@@ -56,57 +59,57 @@ if __name__ == "__main__":
     def random_city_type() -> str:
         return random.choice(["Urban", "Metropolitian"])
 
-    # Enriquecer com features
-    df = (
-        df.withColumn("Delivery_person_Age", 2025 - expr("substring(date_birth, 1, 4)").cast("int"))
-        .withColumnRenamed("order_id", "ID")
-        .withColumnRenamed("driver_key", "Delivery_person_ID")
-        .withColumnRenamed("delivery_latitude", "Delivery_location_latitude")
-        .withColumnRenamed("delivery_longitude", "Delivery_location_longitude")
-        .withColumnRenamed("restaurant_latitude", "Restaurant_latitude")
-        .withColumnRenamed("restaurant_longitude", "Restaurant_longitude")
-        .withColumnRenamed("order_date", "Order_Date")
-        .withColumnRenamed("order_time", "Time_Orderd")
-        .withColumnRenamed("pickup_time", "Time_Order_picked")
-        .withColumnRenamed("vehicle_type", "Type_of_vehicle")
-        .withColumnRenamed("vehicle_condition", "Vehicle_condition")
-        .withColumn("Weatherconditions", random_weather())
-        .withColumn("Road_traffic_density", random_traffic())
-        .withColumn("multiple_deliveries", random_multi())
-        .withColumn("Type_of_order", random_type_of_order())
-        .withColumn("Festival", lit("No"))
-        .withColumn("City", random_city_type())
-    )
+    # # Enriquecer com features
+    # df = (
+    # df.withColumn("Delivery_person_Age", 2025 - expr("substring(date_birth, 1, 4)").cast("int"))
+    #     .withColumnRenamed("order_id", "ID")
+    #     .withColumnRenamed("driver_key", "Delivery_person_ID")
+    #     .withColumnRenamed("delivery_latitude", "Delivery_location_latitude")
+    #     .withColumnRenamed("delivery_longitude", "Delivery_location_longitude")
+    #     .withColumnRenamed("restaurant_latitude", "Restaurant_latitude")
+    #     .withColumnRenamed("restaurant_longitude", "Restaurant_longitude")
+    #     .withColumnRenamed("order_date", "Order_Date")
+    #     .withColumnRenamed("order_time", "Time_Orderd")
+    #     .withColumnRenamed("pickup_time", "Time_Order_picked")
+    #     .withColumnRenamed("vehicle_type", "Type_of_vehicle")
+    #     .withColumnRenamed("vehicle_condition", "Vehicle_condition")
+    #     .withColumn("Weatherconditions", random_weather())
+    #     .withColumn("Road_traffic_density", random_traffic())
+    #     .withColumn("multiple_deliveries", random_multi())
+    #     .withColumn("Type_of_order", random_type_of_order())
+    #     .withColumn("Festival", lit("No"))
+    #     .withColumn("City", random_city_type())
+    # )
 
-    # Selecionar e reordenar colunas no formato desejado
-    df_gold = df.select(
-        "ID",
-        "Delivery_person_ID",
-        "Delivery_person_Age",
-        "Delivery_person_Ratings",
-        "Restaurant_latitude",
-        "Restaurant_longitude",
-        "Delivery_location_latitude",
-        "Delivery_location_longitude",
-        "Order_Date",
-        "Time_Orderd",
-        "Time_Order_picked",
-        "Weatherconditions",
-        "Road_traffic_density",
-        "Vehicle_condition",
-        "Type_of_order",
-        "Type_of_vehicle",
-        "multiple_deliveries",
-        "Festival",
-        "City",
-        "Time_taken",
-    )
+    # # Selecionar e reordenar colunas no formato desejado
+    # df_gold = df.select(
+    #     "ID",
+    #     "Delivery_person_ID",
+    #     "Delivery_person_Age",
+    #     "Delivery_person_Ratings",
+    #     "Restaurant_latitude",
+    #     "Restaurant_longitude",
+    #     "Delivery_location_latitude",
+    #     "Delivery_location_longitude",
+    #     "Order_Date",
+    #     "Time_Orderd",
+    #     "Time_Order_picked",
+    #     "Weatherconditions",
+    #     "Road_traffic_density",
+    #     "Vehicle_condition",
+    #     "Type_of_order",
+    #     "Type_of_vehicle",
+    #     "multiple_deliveries",
+    #     "Festival",
+    #     "City",
+    #     "Time_taken",
+    # )
 
     # Salvar no Hive como tabela Delta
-    df_gold.write.format("delta").mode("overwrite").saveAsTable("uber.gold_delivery_ml_ready")
+    # df_gold.write.format("delta").mode("overwrite").saveAsTable("uber.gold_delivery_ml_ready")
 
     # Persistindo em Parquet (modo overwrite para sobrescrever se já existir)
-    df_gold.write.mode("overwrite").parquet("s3a://gold/delivery_dataset/")
+    # df_gold.write.mode("overwrite").parquet("s3a://gold/delivery_dataset/")
 
     # Persistindo em CSV (com header, separador padrão, e sobrescrevendo também)
     # df_gold.toPandas().to_csv(
