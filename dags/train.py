fed61f3ad824c33eca5a81e3f10e3b5b6337aff8
GersonRS <gersonrodriguessantos8@gmail.com>
2026-07-20 01:50:11 -0300

    feat: adjust schedule parameter

--- dags/train.py
+++ dags/train.py
@@ -6,14 +6,9 @@ from typing import Any
 from airflow.decorators import dag, task, task_group
 from airflow.operators.empty import EmptyOperator
 from airflow.sdk import Asset
+from dags.utils.constants import default_args
 from mlflow_provider.hooks.client import MLflowClientHook
-from mlflow_provider.operators.registry import (
-    CreateModelVersionOperator,
-    CreateRegisteredModelOperator,
-    TransitionModelVersionStageOperator,
-)
 from sklearn.linear_model import LogisticRegression
-from utils.constants import default_args
 
 FILE_PATH = "features.parquet"
 
@@ -103,80 +98,7 @@ def train() -> None:  # noqa: C901
         run_name="ModelLR-{{ ts_nodash }}",
     )
 
-    @task_group
-    def register_model() -> None:
-        @task.branch
-        def check_if_model_already_registered(reg_model_name: str) -> Any:
-            "Get information about existing registered MLFlow models."
-
-            mlflow_hook = MLflowClientHook(mlflow_conn_id=MLFLOW_CONN_ID, method="GET")
-            get_reg_model_response = mlflow_hook.run(
-                endpoint="api/2.0/mlflow/registered-models/get",
-                request_params={"name": reg_model_name},
-            ).json()
-
-            if "error_code" in get_reg_model_response:
-                if get_reg_model_response["error_code"] == "RESOURCE_DOES_NOT_EXIST":
-                    reg_model_exists = False
-                else:
-                    raise ValueError(
-                        f"Error when checking if model is registered: {get_reg_model_response['error_code']}"  # noqa: E501
-                    )
-            else:
-                reg_model_exists = True
-
-            if reg_model_exists:
-                return "register_model.model_already_registered"
-            else:
-                return "register_model.create_registered_model"
-
-        model_already_registered = EmptyOperator(task_id="model_already_registered")
-
-        create_registered_model = CreateRegisteredModelOperator(
-            mlflow_conn_id=MLFLOW_CONN_ID,
-            task_id="create_registered_model",
-            name=REGISTERED_MODEL_NAME,
-            tags=[
-                {"key": "model_type", "value": "regression"},
-                {"key": "data", "value": "iris"},
-            ],
-        )
-
-        create_model_version = CreateModelVersionOperator(
-            mlflow_conn_id=MLFLOW_CONN_ID,
-            task_id="create_model_version",
-            name=REGISTERED_MODEL_NAME,
-            source="s3://"
-            + MLFLOW_ARTIFACT_BUCKET
-            + "/"
-            + "{{ ti.xcom_pull(task_ids='train_model') }}",
-            run_id="{{ ti.xcom_pull(task_ids='train_model') }}",
-            trigger_rule="none_failed",
-        )
-
-        transition_model = TransitionModelVersionStageOperator(
-            mlflow_conn_id=MLFLOW_CONN_ID,
-            task_id="transition_model",
-            name=REGISTERED_MODEL_NAME,
-            version="{{ ti.xcom_pull(task_ids='register_model.create_model_version')['model_version']['version'] }}",  # noqa: E501
-            stage="Staging",
-            archive_existing_versions=True,
-        )
-
-        (
-            check_if_model_already_registered(reg_model_name=REGISTERED_MODEL_NAME)
-            >> [model_already_registered, create_registered_model]
-            >> create_model_version
-            >> transition_model
-        )
-
-    (
-        start
-        >> [fetched_feature_df, fetched_experiment_id]
-        >> model_trained
-        >> register_model()
-        >> end
-    )
+    (start >> [fetched_feature_df, fetched_experiment_id] >> model_trained >> end)
 
 
 train()
